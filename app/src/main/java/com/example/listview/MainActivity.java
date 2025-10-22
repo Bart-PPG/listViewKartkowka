2b95f0df4821046f0b6f94d3da8d6f062cb4757c
Bart-PPG <Bartek.nitek@onet.pl>
2025-10-22 09:43:42 +0200

    dodawanie liczb

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -56,7 +56,9 @@ public class MainActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
+                    int liczba = Integer.parseInt(editText.getText().toString());
+                    liczbyWylosowaneArrayList.add(liczba);
+                    arrayAdapter.notifyDataSetChanged();
                 }
             });
 
